b25224e49b1fe3ce4fc0395893b500e52dfe422c
ROGER CABRAL SILVA <cabralroger159@gmail.com>
2024-06-19 09:14:13 -0300

    Criação da conexão do banco de dados psicomind

--- Psicomind/PsicomindClass/Banco.cs
+++ Psicomind/PsicomindClass/Banco.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace PsicomindClass
 {
-    public class Banco
+    public static class Banco
     {
+        public static MySqlCommand Abrir()
+        {
+            MySqlCommand cmd = new MySqlCommand();
+            // Dados da Conexão
+            string strconn = @"server=127.0.0.1;database=ComercialDB;user id=root;password=";
+            MySqlConnection cn = new MySqlConnection(strconn);
+            try
+            {
+                cn.Open();
+                cmd.Connection = cn;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return cmd;
+        }
     }
 }
